159f12dc2fb3ce5746761a10906060191976c862
scrabill <shannon.crabill@gmail.com>
2020-11-21 18:24:38 -0500

    Resolves file conflicts

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
-## ConvertKit Software Engineer Project
+# My Dream Music Festival
 
-Welcome! ConvertKit's homework assignment is meant to be an approximation of work at ConvertKit. The tasks are a bit contrived,
-but the concepts are things you may deal with on a day to day basis. We respect your time, and know that going through the hiring
-process at multiple companies can be time consuming, so we request that you don't spend much more than two hours completing the assignment.
-If you finish faster, great! If you get part of the way through, that's no problem either. You have the opportunity to explain any decisions
-you made based on these constraints when you create your pull request.
+A work in progress experiment leveraging the [Spotify API](https://developer.spotify.com/documentation/web-api/)
 
-## Instructions
+Coming soon. 
 
-We've set up `create-react-app` in this repo, and you should be able to get dependencies installed with `yarn install` and the app running with `yarn run`. If not, let us know and we'll help out. From here, we want you to build an app using the Spotify API and these features:
-
-- A user can search for an artist, and see results as they type
-- A user can click on an artist to see the artist's top tracks
-
-Everything about the implementation is up to you, including the use of `create-react-app`. If you're more comfortable with something else, feel free to use that. We recommend using the included [Spotify Web API JS](https://github.com/JMPerez/spotify-web-api-js) library to make requests to Spotify. Example authentication code is provided in `App.js`.
-
-Once you're finished, create a Pull Request with your changes in this repo. Write a description of your code changes, including any decisions or tradeoffs you made. Since two hours is not a lot of time, include anything you would have done or changed given more time.
-
-Have any questions? Please ask!
+🎧
--- src/App.js
+++ src/App.js
@@ -1,83 +1,10 @@
-import React, { useState } from "react";
-import Spotify from "spotify-web-api-js";
-import "./App.css";
-
-// Returns an access token that can be used in the `setAccessToken` function of the
-// spotify-web-api library. Example:
-// const accessToken = await getAccessToken();
-// const spotify = new Spotify();
-// spotify.setAccessToken(accessToken)
-async function getAccessToken() {
-  const response = await fetch(
-    "https://assignment.convertkit.dev/.netlify/functions/auth-spotify"
-  );
-
-  const json = await response.json();
-
-  console.log(json.access_token)
-
-  return json.access_token;
-}
+import './App.css';
 
 function App() {
-
-  const [input, setInput] = useState("")
-  const [artists, setArtists] = useState([])
-  const spotify = new Spotify();
-  spotify.setAccessToken(getAccessToken()) // Access token may need to be hard coded for demo purposes
-
-  console.log(spotify)
-
-  function handleOnChange(e) {
-        
-    e.preventDefault();
-
-    setInput(e.target.value)
-
-    findArtist()
-  
-  }
-
-  function findArtist() {
-
-    let searchQuery = {input}.input
-
-    spotify.searchArtists(searchQuery).then(
-      function (data) {
-        console.log(input)
-        console.log(data.artists.items);
-        setArtists(data.artists.items)
-        renderArtists()
-      },
-      function (err) {
-        console.error(err);
-      }
-    );
-  }
-  
-  function renderArtists() {
-    console.log(artists)
-
-    return artists.map(artist => <p><a href={artist.external_urls.spotify}>{artist.name}</a></p>)
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <h1>Search for an artist</h1>
-        <h2>Showing results for: "{input}"</h2>
-        
-        <form>
-            <input type="text" name="input" id="input" onChange={(e) => handleOnChange(e)} />
-        </form>
-
-
-      {renderArtists()}
-
-
-      </header>
-    </div>
+    <main>
+      <h1>My Dream Music Festival</h1>
+    </main>
   );
 }
 
